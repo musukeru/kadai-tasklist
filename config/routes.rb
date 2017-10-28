2cd9e422d9320bd18ba1bd27980a3ce523470341
Hiroshi Yoshioka <hiroshi.yoshioka@g.softbank.co.jp>
2017-10-28 13:10:08 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
 Rails.application.routes.draw do
+
+  get 'users/edit'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root to: 'tasks#index'
+ 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
 
   resources :tasks
+  resources :users
 end
