ab15cc428bd97855aec7ac76db2a8f00c2b07206
Hiroshi Yoshioka <hiroshi.yoshioka@g.softbank.co.jp>
2017-10-29 12:36:19 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :new, :create]
    before_action :set_task, only: [:show, :edit, :update, :destroy]
-     def index
-          @tasks = Task.order(created_at: :desc).page(params[:page]).per(25)
+  def index
+    if logged_in?
+      @user = current_user
+      @task = current_user.tasks.build  # form_for 用
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    end
   end
-
   def show
      @task = Task.find(params[:id])
   end
@@ -13,12 +17,12 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
       redirect_to @task
     else
+      @task = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が作成されませんでした'
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'tasks/create'
+
+  get 'tasks/destroy'
+
   get 'users/edit'
 
   get 'users/index'
